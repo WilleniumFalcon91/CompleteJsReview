dab4058a0667afa748bda478639bd83f93feb09e
William Elliott <wcelliott1991@gmail.com>
2017-10-30 12:02:16 -0400

    variable mutation and prompt review

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+//Lecture: variables
+
+// var name = 'John';
+// console.log(name);
+
+// var lastName = "Smith";
+// console.log(lastName); 
+
+// var age = 26;
+// console.log(26);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+//Lecture: variables 2
 var name = 'John';
-console.log(name);
-
-var lastName = "Smith";
-console.log(lastName); 
-
 var age = 26;
-console.log(26);
 
-var fullAge = true;
-console.log(fullAge);
+console.log(name + age);
+
+var job, isMarried; 
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is ' + age + " years old and works as a " + job + ". When asked is he is married, he answered: " + isMarried);
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
