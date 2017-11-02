7c083f5d54175212686a027537e16c1d706c98f4
William Elliott <wcelliott1991@gmail.com>
2017-11-02 15:26:06 -0400

    scope chain

--- script.js
+++ script.js
@@ -235,41 +235,30 @@
 // Lecture: Hoisting
 
 //function declaration hoisting
-calculateAge(1990); 
+// calculateAge(1990); 
 
-function calculateAge(year) {
-    console.log(2017 - year);
-}
+// function calculateAge(year) {
+//     console.log(2017 - year);
+// }
 
 //function expression hoisting 
-var retirement = function(year) {
-    console.log(65 - (2017 - year));
-}
+// var retirement = function(year) {
+//     console.log(65 - (2017 - year));
+// }
 
-retirement(1991);
+// retirement(1991);
 
 //variable hoisting and scope
-var age = 25;
-console.log(age);
+// var age = 25;
+// console.log(age);
 
-function foo() {
-    var age = 65;
-    console.log(age);
-}
+// function foo() {
+//     var age = 65;
+//     console.log(age);
+// }
 
-foo();
-console.log(age);
-
-
-
-
-
-
-
-
-
-
-
+// foo();
+// console.log(age);
 
 
 ///////////////////////////////////////
@@ -278,26 +267,26 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
+
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + " " + b + " " + c);
+//     }
+// }
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -307,15 +296,17 @@ function first() {
 
     function second() {
         var c = 'Hey!';
-        third()
+        third();
+
+        function third() {
+            var d = 'John';
+            console.log(a + b + c + d);
+        }
     }
 }
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
+
+
 
 
 
